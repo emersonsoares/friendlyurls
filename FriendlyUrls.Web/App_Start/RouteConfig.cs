acd8a37fb78dd155551f018e2277a88321a2909f
Emerson Soares <on.emersonsoares@gmail.com>
2014-02-13 08:41:16 -0200

    Controller and Routing for Notices

--- FriendlyUrls.Web/App_Start/RouteConfig.cs
+++ FriendlyUrls.Web/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace FriendlyUrls.Web
@@ -13,11 +9,13 @@ namespace FriendlyUrls.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute(name: "Notices",
+                            url: "{controller}/{slug}",
+                            defaults: new { controller = "Notices", action = "Details" });
+
+            routes.MapRoute(name: "Default",
+                            url: "{controller}/{action}/{id}",
+                            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
         }
     }
 }
---
+++ FriendlyUrls.Web/Controllers/NoticesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web.Mvc;
+using FriendlyUrls.Web.Models;
+
+namespace FriendlyUrls.Web.Controllers
+{
+    public class NoticesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public NoticesController()
+        {
+            _db = new ApplicationDbContext();
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            return View(await _db.Notices.ToListAsync());
+        }
+
+        public async Task<ActionResult> Details(string slug)
+        {
+            if (string.IsNullOrEmpty(slug))
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var notice = await (from n in _db.Notices where n.Slug == slug select n).FirstOrDefaultAsync();
+
+            if (notice == null)
+                return HttpNotFound();
+
+            return View(notice);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                _db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
