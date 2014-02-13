c50d2360b558db50f07c4a2a16d0ca459ff25a41
Emerson Soares <on.emersonsoares@gmail.com>
2014-02-13 08:03:14 -0200

    Initial Project

---
+++ FriendlyUrls.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FriendlyUrls.Web.Startup))]
+namespace FriendlyUrls.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
